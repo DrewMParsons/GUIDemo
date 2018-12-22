9932183d03428b579a4cd062d64fc2aa854e1c10
Lumpanous <mongoht@gmail.com>
2018-12-22 13:30:19 -0500

    added radio buitton

--- src/guidemo/FXMLDocumentController.java
+++ src/guidemo/FXMLDocumentController.java
@@ -7,7 +7,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
 /**
  * FXML Controller class
@@ -31,7 +34,30 @@ public class FXMLDocumentController implements Initializable {
     private ChoiceBox choiceBox;
     @FXML
     private Label choiceBoxLabel;
-
+    
+    //Itmes for Combo BOx
+    @FXML 
+    private ComboBox comboBox;
+    @FXML
+    private Label comboBoxLabel;
+    
+    //Items for Radio Button Boc
+    @FXML
+    private RadioButton phpRadioButton;
+    
+    @FXML
+    private RadioButton javaRadioButton;
+    
+    @FXML
+    private RadioButton cSharpRadioButton;
+    
+    @FXML
+    private RadioButton cPlusPlusRadioButton;
+    
+    @FXML 
+    private Label radioButtonLabel;
+    
+    private ToggleGroup favLangToggleGroup;
      /**
      * This will update Label for Choicebox
      */
@@ -45,8 +71,7 @@ public class FXMLDocumentController implements Initializable {
      * This is for Checkbox
      */
      
-
-    @FXML
+    
     public void pizzaOrderButtonPushed() {
         
         String order = "Toppings are:";
@@ -63,10 +88,32 @@ public class FXMLDocumentController implements Initializable {
         
     }
     
-    
+    /**
+     * This will update comboboxlabel when combobox is changed
+     */
     /**
      * Initializes the controller class.
      */
+    
+    public void comboBoxWasUpdated(){
+        this.comboBoxLabel.setText("Course selected:  \n" + comboBox.getValue().toString());
+    }
+    /**
+     *  Method to update radiobutton label
+     */
+    public void radioButtonChanged(){
+        if(this.favLangToggleGroup.getSelectedToggle().equals(this.phpRadioButton))
+            radioButtonLabel.setText("The item selected is PHP");
+        if(this.favLangToggleGroup.getSelectedToggle().equals(this.javaRadioButton))
+            radioButtonLabel.setText("The item selected is Java");
+        if(this.favLangToggleGroup.getSelectedToggle().equals(this.cSharpRadioButton))
+            radioButtonLabel.setText("The item selected is C#");
+        if(this.favLangToggleGroup.getSelectedToggle().equals(this.cPlusPlusRadioButton))
+            radioButtonLabel.setText("The item selected is C++");
+        
+    }
+    
+    
       @Override
     public void initialize(URL url, ResourceBundle rb) {
         pizzaOrderLabel.setText("");
@@ -79,6 +126,20 @@ public class FXMLDocumentController implements Initializable {
         choiceBox.getItems().add("bananas");
         choiceBox.getItems().addAll("oranges","pears","strawberries");
         choiceBox.setValue("apples");
+        
+        //Thse items are for configuring the ComboBox
+        comboBoxLabel.setText("");
+        comboBox.getItems().addAll("COMP1030","COMP1008","MGMT2010","MGMT2030");
+        
+        //Thes configure Radio Buttons
+        radioButtonLabel.setText("");
+        favLangToggleGroup = new ToggleGroup();
+        this.phpRadioButton.setToggleGroup(favLangToggleGroup);
+        this.javaRadioButton.setToggleGroup(favLangToggleGroup);
+        this.cSharpRadioButton.setToggleGroup(favLangToggleGroup);
+        this.cPlusPlusRadioButton.setToggleGroup(favLangToggleGroup);
+        
+        
     } 
     
 }
